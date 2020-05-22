07217fa6d1bb1b854fd68f4d66cfb1d236dbfde2
Andrew Wagenmaker <ajwagen@cs.washington.edu>
2020-05-22 07:57:14 -0700

    Initial Commit

---
+++ screen_com/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  screen_com
+//
+//  Created by Andrew Wagenmaker on 5/22/20.
+//  Copyright © 2020 Andrew Wagenmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ screen_comTests/screen_comTests.swift
@@ -0,0 +1,34 @@
+//
+//  screen_comTests.swift
+//  screen_comTests
+//
+//  Created by Andrew Wagenmaker on 5/22/20.
+//  Copyright © 2020 Andrew Wagenmaker. All rights reserved.
+//
+
+import XCTest
+@testable import screen_com
+
+class screen_comTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
